07ee6ebc2061675df0c14ee568b7e7ff0f0beb3b
Oleg <Kornyakovo@gmail.com>
2022-05-12 04:18:16 +0400

    feat: add run button animation

---
+++ animations.js
@@ -0,0 +1,13 @@
+let codeRunButtons = document.querySelectorAll('.code__run-button')
+codeRunButtons.forEach(element => {
+    element.addEventListener("mouseenter", ()=>{codeRunButtonsActive(element)}, false);
+    element.addEventListener("mouseout", ()=>{codeRunButtonsActive(element)}, false);
+});
+
+function codeRunButtonsActive (element){
+    element.classList.toggle('code__run-button--active')
+    element.querySelector('h2').classList.toggle('code__run-button-header--active')
+}
+
+// addEventListener("mouseenter", (e)=>{console.log(e)}, false);
+
